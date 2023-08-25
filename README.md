e5c35c63a9d745dc6c2a1a4c46b8bbe1bf9c6e3a
filipnet <benedikt.filip@filipnet.de>
2023-08-25 22:08:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ups-json-publisher
 UPS Data Publisher for MQTT: Capture UPS details using upsc, format as JSON, and send to MQTT broker.
+
+## Usage
+1. **Clone the Repository:**
+```
+git clone https://github.com/filipnet/ups-json-publisher.git
+cd ups-json-publisher
+```
+
+2. **Configure MQTT Broker Settings:**
+Copy the example configuration file:
+```
+cp config.sh.example config.sh
+```
+Open config.sh in a text editor (e.g., nano, vim) and replace placeholders with your actual MQTT broker information.
+
+3. **Run the Script:**
+Make sure the required dependencies are installed: upsc (part of Network UPS Tools) and mosquitto_pub (part of Mosquitto MQTT client).
+
+Run the script with the device name as an argument:
+```
+./mqtt_upsc_script.sh <device_name>
+```
+Important: Protect Your Configuration:
+
+The config.sh file contains sensitive information. Make sure not to expose your credentials by adding config.sh to the .gitignore file. Never commit the actual config.sh to the repository.
+
+## Dependencies
+- Network UPS Tools
+- Mosquitto MQTT Client
+
+## License
+This project is licensed under the BSD 3-Clause License.
---
+++ mqtt_upsc_script.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+################################################################################
+# This script retrieves UPS (Uninterruptible Power Supply) information using the
+# upsc command, converts it to JSON format, and publishes it to an MQTT broker.
+#
+# Usage: ./mqtt_upsc_script.sh <device_name>
+#
+# The script requires the upsc command to be installed, and it uses mosquitto_pub
+# to publish the JSON output to an MQTT broker. Configuration details are kept
+# in the 'config.sh' file. Make sure to set the appropriate MQTT broker settings
+# in the 'config.sh' file before running the script.
+################################################################################
+
+# Check if the script was called with a device name as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <device_name>"
+    exit 1
+fi
+
+# Extract device name from the argument
+device_name=$1
+
+# Execute the `upsc` command and store the output in a variable
+upsc_output=$(upsc "$device_name" 2>/dev/null)
+
+# Check if the device is reachable
+if [ -z "$upsc_output" ]; then
+    echo "The device '$device_name' could not be reached."
+    exit 1
+fi
+
+# Convert output to JSON format
+json_output="{"
+while IFS=: read -r key value; do
+    key=$(echo "$key" | sed 's/^[ \t]*//;s/[ \t]*$//')
+    value=$(echo "$value" | sed 's/^[ \t]*//;s/[ \t]*$//')
+    
+    # Check if the value can be converted to a number
+    if [[ "$value" =~ ^[0-9]+\.?[0-9]*$ ]]; then
+        json_output+="\"$key\":$value,"
+    else
+        json_output+="\"$key\":\"$value\","
+    fi
+done <<< "$upsc_output"
+json_output="${json_output%,}"  # Remove the trailing comma
+json_output+="}"
+
+# Display JSON output
+echo "$json_output"
+
+# Load configuration variables
+source config.sh
+
+# Publish JSON output to MQTT broker
+echo "$json_output" | mosquitto_pub -h "$mqtt_broker" -p "$mqtt_port" -u "$mqtt_username" -P "$mqtt_password" -i "$mqtt_client_id" -t "$mqtt_topic" -l
